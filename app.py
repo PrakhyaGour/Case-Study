8675637bb322af1ff1551a7cd855604c21ef70d2
PrakhyaGour <prakhyagour07@gmail.com>
2020-02-07 18:15:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return " check if it is working or not....why"
+    return " hi"
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0', port=8000)
