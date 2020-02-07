9376771e3b2c0b4759943681820fe826eb0fc560
PrakhyaGour <prakhyagour07@gmail.com>
2020-02-07 17:17:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello world of Kubernetes with flux!!!!!! check if it is working"
+    return " check if it is working"
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0', port=8000)
