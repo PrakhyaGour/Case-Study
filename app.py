5ddb1823192cd95e5157e726fce3229696f484ab
PrakhyaGour <prakhyagour07@gmail.com>
2020-02-10 10:15:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Check if flux is working with git and begin with app mesh"
+    return "Check if flux is working with git and begin with app mesh helllo"
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0', port=8000)
