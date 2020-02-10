6bcb2415dedaf12c88ec1f111a4c79ca5b0bebfd
PrakhyaGour <prakhyagour07@gmail.com>
2020-02-10 13:06:07 +0530

    Update README.d

--- README.d
+++ README.d
@@ -11,14 +11,6 @@ We believe in GitOps:
 2-Git as the single place where we operate all environments 
 3-All changes are observable/verifiable 
 
-For the canary deployment will be using app-mesh of AWS in our cae-study
-
-What is App-mesh?
-
-App Mesh makes it easy to run microservices by providing consistent visibility and network traffic controls for every microservice in an application. App Mesh separates the logic needed for monitoring and controlling communications into a proxy that runs next to every microservice. App Mesh removes the need to coordinate across teams or update application code to change how monitoring data is collected or traffic is routed. This allows you to quickly pinpoint the exact location of errors and automatically re-route network traffic when there are failures or when code changes need to be deployed.
-
-You can use App Mesh with AWS Fargate, Amazon Elastic Container Service (ECS), Amazon Elastic Container Service for Kubernetes (EKS), and Kubernetes on EC2 to better run containerized microservices at scale. App Mesh uses Envoy, an open source proxy, making it compatible with a wide range of AWS partner and open source tools for monitoring microservices
-
 Prerequisites:
 -AWS account
 -EKS cluster created
